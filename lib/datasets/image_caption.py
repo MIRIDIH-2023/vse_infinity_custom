c1783d34d258e0ce7595dcdf582f80246eda5bd1
naye0319 <naye03@naver.com>
2023-09-08 05:30:50 +0900

    as

--- lib/datasets/image_caption.py
+++ lib/datasets/image_caption.py
@@ -8,10 +8,187 @@ from imageio import imread
 import random
 import json
 import cv2
+from PIL import Image
+import pickle
 
 import logging
 
 logger = logging.getLogger(__name__)
+
+class CustomRawImageDataset(data.Dataset):
+    """
+    Load precomputed captions and image features
+    Possible options: f30k_precomp, coco_precomp
+    """
+
+    def __init__(self, data_path, data_name, data_split, tokenzier, opt, train):
+        ############################ custom options #######################
+        self.image_len = 40000 #train length
+        self.validation_len = 2000 #validation length
+        self.im_div = 5 #num of captions
+        self.image_root = '/content/drive/MyDrive/images'
+        self.json_root = '/content/drive/MyDrive/data_temp/data_list.pickle'
+        ###################################################################
+        
+        self.opt = opt
+        self.train = train
+        self.data_path = data_path
+        self.data_name = data_name
+        self.tokenizer = tokenzier
+
+        # Read Captions
+        self.captions = self.make_captions()
+
+        # Set related parameters according to the pre-trained backbone **
+        assert 'backbone' in opt.precomp_enc_type
+
+        self.backbone_source = opt.backbone_source
+        self.base_target_size = 256
+        self.crop_ratio = 0.875
+        self.train_scale_rate = 1
+        if hasattr(opt, 'input_scale_factor') and opt.input_scale_factor != 1:
+            self.base_target_size = int(self.base_target_size * opt.input_scale_factor)
+            logger.info('Input images are scaled by factor {}'.format(opt.input_scale_factor))
+        if 'detector' in self.backbone_source:
+            self.pixel_means = np.array([[[102.9801, 115.9465, 122.7717]]])
+        else:
+            self.imagenet_mean = [0.485, 0.456, 0.406]
+            self.imagenet_std = [0.229, 0.224, 0.225]
+    
+    def make_captions(self):
+        with open(self.json_root,'rb') as file:
+            json_list = pickle.load(file) 
+        return_list = []
+        for _index in range(len(json_list)):
+            
+            for _ in range(self.im_div):
+                keyword = json_list[_index]['keyword']
+                
+                if len(keyword) == 0: #if has error, just unk token
+                    keyword = ['[UNK]']
+                    
+                random.shuffle(keyword)
+                keyword = ' '.join(keyword)
+                
+                return_list.append(keyword)
+
+            """
+            text = ""
+
+            for j in range(len(json_list[_index]['form'])):
+                if type(json_list[_index]['form'][j]['text']) == str:
+                    text += json_list[_index]['form'][j]['text'] + "\n"
+            """
+
+        return return_list
+        
+        
+    def __getitem__(self, index):
+        img_index = index // self.im_div
+        caption = self.captions[index]
+        caption_tokens = self.tokenizer.basic_tokenizer.tokenize(caption)
+
+        # Convert caption (string) to word ids (with Size Augmentation at training time).
+        target = process_caption(self.tokenizer, caption_tokens, self.train)
+
+        path = f"thumnail_image_{img_index}.png"
+        im_in = np.array(Image.open(os.path.join(self.image_root, path)).convert('RGB'))
+        
+        processed_image = self._process_image(im_in)
+        image = torch.Tensor(processed_image)
+        image = image.permute(2, 0, 1)
+        return image, target, index, img_index
+
+    def __len__(self):
+        _len = self.image_len
+        if self.train:
+            _len = self.image_len - self.validation_len
+        else:
+            _len = self.validation_len
+        return _len * self.im_div #image data length * num of captions
+
+    def _process_image(self, im_in):
+        """
+            Converts an image into a network input, with pre-processing including re-scaling, padding, etc, and data
+        augmentation.
+        """
+        if len(im_in.shape) == 2:
+            im_in = im_in[:, :, np.newaxis]
+            im_in = np.concatenate((im_in, im_in, im_in), axis=2)
+
+        if 'detector' in self.backbone_source:
+            im_in = im_in[:, :, ::-1]
+        im = im_in.astype(np.float32, copy=True)
+
+        if self.train:
+            target_size = self.base_target_size * self.train_scale_rate
+        else:
+            target_size = self.base_target_size
+
+        # 2. Random crop when in training mode, elsewise just skip
+        if self.train:
+            crop_ratio = np.random.random() * 0.4 + 0.6
+            crop_size_h = int(im.shape[0] * crop_ratio)
+            crop_size_w = int(im.shape[1] * crop_ratio)
+            processed_im = self._crop(im, crop_size_h, crop_size_w, random=True)
+        else:
+            processed_im = im
+
+        # 3. Resize to the target resolution
+        im_shape = processed_im.shape
+        im_scale_x = float(target_size) / im_shape[1]
+        im_scale_y = float(target_size) / im_shape[0]
+        processed_im = cv2.resize(processed_im, None, None, fx=im_scale_x, fy=im_scale_y,
+                                  interpolation=cv2.INTER_LINEAR)
+
+        if self.train:
+            if np.random.random() > 0.5:
+                processed_im = self._hori_flip(processed_im)
+
+        # Normalization
+        if 'detector' in self.backbone_source:
+            processed_im = self._detector_norm(processed_im)
+        else:
+            processed_im = self._imagenet_norm(processed_im)
+
+        return processed_im
+
+    def _imagenet_norm(self, im_in):
+        im_in = im_in.astype(np.float32)
+        im_in = im_in / 255
+        for i in range(im_in.shape[-1]):
+            im_in[:, :, i] = (im_in[:, :, i] - self.imagenet_mean[i]) / self.imagenet_std[i]
+        return im_in
+
+    def _detector_norm(self, im_in):
+        im_in = im_in.astype(np.float32)
+        im_in -= self.pixel_means
+        return im_in
+
+    @staticmethod
+    def _crop(im, crop_size_h, crop_size_w, random):
+        h, w = im.shape[0], im.shape[1]
+        if random:
+            if w - crop_size_w == 0:
+                x_start = 0
+            else:
+                x_start = np.random.randint(w - crop_size_w, size=1)[0]
+            if h - crop_size_h == 0:
+                y_start = 0
+            else:
+                y_start = np.random.randint(h - crop_size_h, size=1)[0]
+        else:
+            x_start = (w - crop_size_w) // 2
+            y_start = (h - crop_size_h) // 2
+
+        cropped_im = im[y_start:y_start + crop_size_h, x_start:x_start + crop_size_w, :]
+
+        return cropped_im
+
+    @staticmethod
+    def _hori_flip(im):
+        im = np.fliplr(im).copy()
+        return im
 
 
 class RawImageDataset(data.Dataset):
@@ -337,7 +514,8 @@ def get_loader(data_path, data_name, data_split, tokenizer, opt, batch_size=100,
                                                   num_workers=num_workers,
                                                   drop_last=drop_last)
     else:
-        dset = RawImageDataset(data_path, data_name, data_split, tokenizer, opt, train)
+        #dset = RawImageDataset(data_path, data_name, data_split, tokenizer, opt, train)
+        dset = CustomRawImageDataset(data_path, data_name, data_split, tokenizer, opt, train)
         data_loader = torch.utils.data.DataLoader(dataset=dset,
                                                   batch_size=batch_size,
                                                   shuffle=shuffle,
