4a9f5d525327ff0f85ffc7cbd6970f08f964bcdc
naye0319 <naye03@naver.com>
2023-09-10 01:53:21 +0900

    as

--- just_testing.py
+++ just_testing.py
@@ -1,23 +1,7 @@
-from PIL import Image
-import matplotlib.pyplot as plt
+import numpy as np
 
-# 이미지를 표시할 화면 크기 설정
-num_images = 20  # 이미지 파일 개수 (0.png부터 20.png까지)
-columns = 5      # 한 행에 표시할 이미지 개수
-rows = (num_images // columns)
+sims = [0,1,2,3,4,5]
 
-# 이미지를 표시할 새로운 화면 생성
-fig = plt.figure(figsize=(10, 8))
+argsorted_sims = np.argsort(sims)[::-1] #[length * 2]
 
-for i in range(num_images):
-    filename = f"{i}.png"
-    try:
-        img = Image.open(filename)
-        fig.add_subplot(rows, columns, i + 1)
-        plt.imshow(img)
-        plt.axis('off')  # 이미지 축 숨기기
-        plt.title(filename)
-    except FileNotFoundError:
-        print(f"파일 {filename}을 찾을 수 없습니다.")
-
-plt.show()
+print(argsorted_sims)
